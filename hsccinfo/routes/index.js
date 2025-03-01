0c1a923931cc0c66f78c906bd6f38ced222c1a91
EthanEz159 <ethanezekiel668@gmail.com>
2025-03-01 16:47:42 +0000

    march update

--- hsccinfo/routes/index.js
+++ hsccinfo/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if (process.env.CONSOLE_DEBUG){
+    console.log("Debugging enabled!")
+  }
   res.render('index', { title: 'Express' });
 });
-
+//Gratuitous comment
 module.exports = router;
